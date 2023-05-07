5bd6306495af6b83f833fd8c9c930ee404caa6c7
Shakhram <shakag6@gmail.com>
2023-05-06 22:33:05 -0400

    landing page background changes every few seconds

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,16 +1,37 @@
 import React, { useEffect, useState } from "react";
 import AutoSearch from "./AutoSearch";
 import "../styles/Landing.css";
-import OneView from "./OneView";
+import image1 from "../images/1.jpg";
+import image2 from "../images/2.jpg";
+import image3 from "../images/3.jpg";
+import image4 from "../images/4.jpg";
+import image5 from "../images/5.jpg";
+import image6 from "../images/6.jpg";
 
 export default function Landing() {
+  const [backgroundImage, setBackgroundImage] = useState(0);
+  const backgrounds = [image1, image2, image3, image4, image5, image6];
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setBackgroundImage((prevIndex) =>
+        prevIndex < backgrounds.length - 1 ? prevIndex + 1 : 0
+      );
+    }, 7000);
+    return () => clearInterval(intervalId);
+  }, [backgrounds.length]);
+
   return (
-    <div className="landing-page">
+    <div
+      className="landing-page"
+      style={{
+        backgroundImage: `url(${backgrounds[backgroundImage]})`,
+      }}
+    >
       <div className="landing-search">
         <h1 className="landing-title">DROP-IN</h1>
 
         <AutoSearch />
-        {/* <OneView/> */}
       </div>
     </div>
   );
